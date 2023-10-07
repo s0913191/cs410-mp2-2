e49a2d36ae707a637ccd3f2e7a5babe91adb5b40
s0913191 <s0913191@gmail.com>
2023-10-07 09:51:04 -0400

    return metapy.index.OkapiBM25(k1=1.25,b=0.79,k3=1)

--- search_eval.py
+++ search_eval.py
@@ -101,10 +101,13 @@ def load_ranker(cfg_file):
     #PASSED!! 0.4291865018197321
     #return metapy.index.OkapiBM25(k1=1.9,b=0.65,k3=6.5)
 
-    #SOTA 0.43082289032771875
+    #PASSED!! 0.43082289032771875
     #return metapy.index.OkapiBM25(k1=1.9,b=0.6,k3=6.5)
 
-    return metapy.index.OkapiBM25(k1=1.9,b=0.6,k3=1)
+    #SOTA 0.43130184126053506
+    #return metapy.index.OkapiBM25(k1=1.9,b=0.6,k3=1)
+
+    return metapy.index.OkapiBM25(k1=1.25,b=0.79,k3=1)
     
     #PASSED!! 0.43080763818161616
     #return metapy.index.OkapiBM25(k1=2,b=0.6,k3=6.5)
